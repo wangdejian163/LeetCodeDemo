60d2cbde5cf68cd9a48699aabe5764d1bd94633b
wangdejian <wangdejian163@163.com>
2018-02-26 12:49:03 +0800

    Two Sum代码实现

--- src/main/java/com/example/leetcode/TwoSum.java
+++ src/main/java/com/example/leetcode/TwoSum.java
@@ -1,5 +1,9 @@
 package com.example.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 /**
  * <p>
  *     two sum
@@ -9,4 +13,33 @@ package com.example.leetcode;
  * @since 2018/2/26
  */
 public class TwoSum {
+    /**
+     Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+     You may assume that each input would have exactly one solution.
+
+     Example:
+     Given nums = [2, 7, 11, 15], target = 9,
+
+     Because nums[0] + nums[1] = 2 + 7 = 9,
+     return [0, 1].
+     给定一个整形数组和一个整数target，如果2个元素的下标满足相加的和为target。 返回这两个元素的下标
+     假定每个输入，都会恰好有一个满足条件的返回结果。
+     *
+     */
+    public int[] twoSum(int [] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        // 实现nums[i] + nums[j] = target
+        for (int i = 0; i< nums.length; i++) {
+            Integer index = map.get(target - nums[i]);
+            // nums数组中2个元素相加等于target就返回索引
+            if (null == index) {
+                map.put(nums[i], i);
+            } else {
+                return new int[]{i, index};
+            }
+        }
+
+        return new int[]{0, 0};
+    }
 }
