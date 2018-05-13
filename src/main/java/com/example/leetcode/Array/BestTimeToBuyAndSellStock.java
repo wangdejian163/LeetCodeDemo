1f6f449c036c1e24f773a396612eaaaed46951a5
wangdejian <wangdejian@bonc.com>
2018-05-13 19:46:27 +0800

    121. 买卖股票的最佳时机

---
+++ src/main/java/com/example/leetcode/Array/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,40 @@
+package com.example.leetcode.Array;
+
+/**
+ * <p>
+ * 121. 买卖股票的最佳时机
+ * </p>
+ *
+ * @author wangdejian
+ * @since 2018/5/13
+ */
+public class BestTimeToBuyAndSellStock {
+    /**
+     * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+     * <p>
+     * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+     * <p>
+     * 注意你不能在买入股票前卖出股票。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: [7,1,5,3,6,4]
+     * 输出: 5
+     * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+     * 示例 2:
+     * <p>
+     * 输入: [7,6,4,3,1]
+     * 输出: 0
+     * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+     */
+    public int maxProfit(int[] prices) {
+        int maxCur = 0; int maxSoFar = 0;
+        for (int i = 1; i< prices.length; i++) {
+            maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+            maxSoFar = Math.max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
+    }
+
+}
